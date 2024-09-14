2a92f43e6d17b860bff27eb07ef326eb2e1fdfc1
윤혁준 <gurwns1540@gmail.com>
2024-09-14 22:29:20 +0900

    [feat] 키클록 연동 테스트 진행 중 1. keycloak 라이브러리 추가 2. 키클록 연동을 위한 설정 값 추가
    [비고]
    현재 연동 테스트 진행 중
    로그인은 성공했으나 이후 token을 가져오는 부분에서 정상적인 동작이 안되어 테스트 진행 중

--- src/main.js
+++ src/main.js
@@ -3,4 +3,71 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Keycloak from "keycloak-js";
+
+// import * as Keycloak from 'keycloak-js'
+import initKeycloakConfig from './keycloak-config.json'
+
+// createApp(App).mount('#app')
+
+const keycloak = new Keycloak(initKeycloakConfig);
+
+init();
+
+function init() {
+    // keycloak.init({
+    //     onLoad: initKeycloakConfig.onLoad
+    // }).then(auth => {
+    //     console.log(auth);
+    //     console.info("Auth OK");
+    //     createApp(App).mount('#app');
+    // }).catch((err) => {
+    //     console.error(err);
+    //     console.error("Auth Failed");
+    // })
+    keycloak.init({
+        onLoad: initKeycloakConfig.onLoad
+    }).then(auth => {
+        const ONE_MINUTE = 60000;
+
+        if(!auth) {
+            window.location.reload();
+        } else {
+            console.info("auth OK");
+        }
+
+        createApp(App).mount('#app');
+
+        localStorage.setItem(`vue-token`, keycloak.token);
+        localStorage.setItem(`vue-refresh-token`, keycloak.refreshToken);
+
+        window.setTimeout(refreshToken.bind(null, keycloak), ONE_MINUTE);
+
+    }).catch(() => {
+        console.error("Auth Failed");
+    });
+}
+
+function refreshToken() {
+    keycloak.updateToken(70).success(refreshed => {
+        if (refreshed) {
+            successRefresh(refreshed);
+        } else {
+            warnRefresh();
+        }
+    }).error(errorRefresh);
+}
+
+function successRefresh(refreshed) {
+    console.debug(`Token refreshed ${refreshed}`);
+}
+
+function warnRefresh() {
+    console.warn(`Token not refreshed, valid for 
+  ${Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000)} seconds`);
+}
+
+function errorRefresh() {
+    console.error('Failed to refresh token');
+}
+
